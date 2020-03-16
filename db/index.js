062e62358050e7108c01aec2cc69b2428da76bf7
zouchengzhuo <405966530@qq.com>
2020-03-16 14:55:45 +0000

    fixbug: get timezone

--- db/index.js
+++ db/index.js
@@ -60,9 +60,9 @@ databases.forEach((database) => {
       idle: pool.idle || 10000,
     },
     timezone: (() => {
-      const timezone = String(0 - new Date().getTimezoneOffset() / 60);
-      return `+${timezone.length < 2 ? (`0${timezone}`) : timezone}:00`;
-    })(), // 获取当前时区并做转换
+			let offset = 0 - new Date().getTimezoneOffset();
+			return (offset >= 0 ? '+' : '-') + (Math.abs(offset/60) + '').padStart(2, '0') + ':00';
+		})()  //获取当前时区并做转换
   });
 
   // 测试是否连接成功
